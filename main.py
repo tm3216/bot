b7f635aaeba3a1ff92785d2ab7cd6115add56027
Тимофей <tmsosin@gmail.com>
2024-05-05 17:40:59 +0300

    ВЫКИНУЛ НАФИГ ЭТОТ ГРЁБАНЫЙ ConversationHandler, И ОНО ЗАРАБОТАЛО!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- main.py
+++ main.py
@@ -18,11 +18,12 @@ async def echo(update: Update, context):
 
 
 async def new_order(update: Update, context):
-    await update.message.reply_text("Введите адрес")
-    return 2
+    reply_keyboard = [['/add_cost', 'Назад']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    await update.message.reply_text("Введите адрес, затем нажмите add_cost, чтобы перейти дальше", reply_markup=markup)
 
 
-async def adress_new_order(update: Update, context):
+async def add_cost(update: Update, context):
     locality = update.message.text
     con = sqlite3.connect("orders")
     cur = con.cursor()
@@ -41,17 +42,8 @@ def main():
     application = Application.builder().token("6944853635:AAHGpI8v4TBjauMG6gx9LAHtzKN_uHa6u6g").build()
     text_handler = MessageHandler(filters.TEXT & filters.COMMAND, echo)
     application.add_handler(CommandHandler("new_order", new_order))
-    application.add_handler(CommandHandler("adress_new_order", adress_new_order))
+    application.add_handler(CommandHandler("add_cost", add_cost))
     application.add_handler(text_handler)
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('echo', echo)],
-        states={
-            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, new_order)],
-            2: [MessageHandler(~filters.TEXT & filters.COMMAND, adress_new_order)]
-        },
-        fallbacks=[CommandHandler('stop', stop)]
-    )
-    application.add_handler(conv_handler)
     application.run_polling()
 
 
